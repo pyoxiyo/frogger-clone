f7227b5edc2b2814aa11714494860c813f07865b
Abdullah Al-Qademi <pyoxiyo@gmail.com>
2018-01-04 15:06:34 +0300

    'final update'

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-frontend-nanodegree-arcade-game
-===============================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+# Frogger clone
+in this game, you play as a boy trying to reach the water, but don't touch any bugs on the way, **for they will eat you alive**.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+# How to launch the game
+open `index.html` with your favourite browser!
+
+# How to play the game
+1- use the arrow keys to move.
+2- if you touch the bugs you lose.
+3- if you reach the water you win.
--- js/app.js
+++ js/app.js
@@ -1,78 +1,102 @@
-var enemyX = -100;
-var enemyY = [220, 140, 60];
-var enemySP = [160, 240, 320];
-var playerX = 200;
-var playerY = 380;
+'use strict';
+const ENEMY_X = -100;
+const ENEMY_Y = [220, 140, 60];
+const ENEMY_SP = [160, 240, 320];
+const ENEMY_IMG = 'images/enemy-bug.png';
+const PLAYER_X = 200;
+const PLAYER_Y = 380;
+const PLAYER_IMG = 'images/char-boy.png';
+const VICTORY_MSG = ["GG", "YOU WIN", "you made it to the water!"];
+const DEATH_MSG = ["oh no...", "YOU DIED", "WASTED"];
+
+// The Enemy class and Player class inherit from this class
+var Char = function (x, y, sprite) {
+    this.x = x;
+    this.y = y;
+    this.sprite = sprite;
+};
+
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.x = enemyX;
-    this.y = enemyY[Math.floor(Math.random() * 3)];
-    this.speed = enemySP[Math.floor(Math.random() * 3)];
+    Char.call(this, ENEMY_X, ENEMY_Y, ENEMY_IMG)
+    // // The Enemies spawn randomly in one of the 3 stone roads
+    this.y = ENEMY_Y[Math.floor(Math.random() * 3)];
+    // Gave the enemies multiple speeds
+    this.speed = ENEMY_SP[Math.floor(Math.random() * 3)];
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // Makes the enemies move
     this.x = this.x + (this.speed * dt);
+    // this if statement's job is to deal with enemies when they move off the edge
     if (this.x > 505) {
-        this.x = enemyX;
-        this.speed = enemySP[Math.floor(Math.random() * 3)];
+        this.x = ENEMY_X;
+        this.speed = ENEMY_SP[Math.floor(Math.random() * 3)];
+        // Enemies will go down a row, unless they are in the lowest row,
+        //  then the enemy posistion will be randomized
         this.y = this.y + 80;
         if (this.y > 220) {
-            this.y = enemyY[Math.floor(Math.random() * 3)];
+            this.y = ENEMY_Y[Math.floor(Math.random() * 3)];
         }
     }
-    // Handles collision between player and enemy
-    if (player.x > this.x - 80 && player.x < this.x + 80 && player.y > this.y - 80 && player.y < this.y + 80) {
-            player.x = playerX;
-            player.y = playerY;
-    }
+    
 
 };
 
-// Draw the enemy on the screen, required method for game
+// Handles collision between player and enemy,
+Enemy.prototype.checkCollisions = function() {
+    // if the player is only 80 units or less away from the enemy,
+    // the game will reset and it count's as a loss.
+    if (player.x > this.x - 80 && player.x < this.x + 80 && player.y > this.y - 80 && player.y < this.y + 80) {
+        player.reset();
+        alert(DEATH_MSG[Math.floor(Math.random() * 3)]);
+    }
+}
+
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
+// The player Class
 var Player = function(){
-    this.sprite = 'images/char-boy.png';
-    this.x = playerX;
-    this.y = playerY;
+    Char.call(this, PLAYER_X, PLAYER_Y, PLAYER_IMG);
 };
 
+// rendering the player character
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 Player.prototype.update = function()  {
-    for(var i = 0; i < allEnemies.length; i++) {
-        if (this.x > allEnemies[i].x - 80 && this.x < allEnemies[i].x + 80 && this.y > allEnemies[i].y - 80 && this.y < allEnemies[i].y + 80) {
-            this.x = playerX;
-            this.y = playerY;       
+    // Winning by reaching the water
+    if (this.y < 50) {
+        this.reset();
+        alert(VICTORY_MSG[Math.floor(Math.random() * 3)]);
     }
+    // Losing by colliding with the enemy
+    for(var i = 0, len = allEnemies.length; i < len; i++) {
+        if (this.x > allEnemies[i].x - 80 && this.x < allEnemies[i].x + 80 && this.y > allEnemies[i].y - 80 && this.y < allEnemies[i].y + 80) {
+            this.reset();
+            alert(DEATH_MSG[Math.floor(Math.random() * 3)]);
+        }
     }
 };
+// Reset the player position
+Player.prototype.reset = function() {
+    this.x = PLAYER_X;
+    this.y = PLAYER_Y;
+}
 
+// Controls
 Player.prototype.handleInput = function(key) {
     this.keyprs = key;
     if (this.keyprs === 'up') {
         this.y = this.y - 80;
     }
-    if (this.keyprs === 'down' && this.y < playerY ) {
+    if (this.keyprs === 'down' && this.y < PLAYER_Y ) {
         this.y = this.y + 80;
     }
     if (this.keyprs === 'left' && this.x !== 0) {
@@ -81,19 +105,14 @@ Player.prototype.handleInput = function(key) {
     if (this.keyprs === 'right' && this.x != 400) {
         this.x = this.x + 100;
     }
-
-    if (this.y < 50) {
-        this.x = playerX;
-        this.y = playerY;
-    }
 };
 
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-var enemy1 = new Enemy();
-var enemy2 = new Enemy();
-var allEnemies = [enemy1, enemy2];
+
+// all enemy objects are in an array called allEnemies
+var allEnemies = [new Enemy(), new Enemy()];
+
 // Place the player object in a variable called player
 var player = new Player();
 
